98f802ebe1a54b948870a649ccf27f5eceb2bbd9
Gaurav Singh <imriitikk7@gmail.com>
2026-05-06 11:00:14 +0530

    update

--- Experiment-7/App.jsx
+++ Experiment-7/App.jsx
@@ -1,10 +1,19 @@
 function App() {
   return (
-    <div className="Student">
-      <h1>Hello Gaurav 🚀</h1>
-      <p>My React journey starts now!</p>
+    <div className="App">
+      <h1>ECE2- React App</h1>
+        <p>This is a simple React application created for ECE2 experiment.</p>
+
     </div>
   );
 }
-
+export default App;
+function App() {
+  return (
+    <div className="Student">
+        <h1>Student Information</h1>
+        <p>Name: Gaurav Singh</p>
+    </div>
+  );
+}
 export default App;
---
+++ Experiment-7/student.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1>
+                Student list
+            </h1>
+            <Student />
+             <Student />
+              <Student />
+        </div>
+    )
+}
